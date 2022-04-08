12c344562877ca963c89477e6c5f2a57641e372f
jefferson <mfljefferson@gmail.com>
2022-04-08 18:50:23 -0300

    local testing

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,27 @@
-use std::fmt::{Display, Formatter};
-use std::str::FromStr;
-use actix_web::{App, HttpResponse, HttpServer, web, get, error, ResponseError};
+use crate::CustomResponseErrors::{ConnectionProblems, InvalidToken};
 use actix_web::http::StatusCode;
 use actix_web::web::Json;
+use actix_web::{error, get, web, App, HttpResponse, HttpServer, ResponseError};
+use serde::Serialize;
 use serde_json::{json, Value};
-use serde::{Serialize};
+use std::fmt::{Display, Formatter};
+use std::str::FromStr;
 use web3::contract::{Contract, Options};
 use web3::types::{Address, U256};
-use crate::CustomResponseErrors::{ConnectionProblems, InvalidToken};
 
 #[derive(Debug)]
 enum CustomResponseErrors {
     InvalidToken(web3::contract::Error),
-    ConnectionProblems(String)
+    ConnectionProblems(String),
 }
 
 impl CustomResponseErrors {
     pub fn name(&self) -> String {
         match self {
             Self::InvalidToken(e) => e.to_string(),
-            Self::ConnectionProblems(error) => error.to_string()
+            Self::ConnectionProblems(error) => error.to_string(),
         }
     }
-
 }
 
 impl Display for CustomResponseErrors {
@@ -47,8 +46,7 @@ impl error::ResponseError for CustomResponseErrors {
             error: self.name(),
         };
 
-        HttpResponse::build(self.status_code())
-            .json(error_response)
+        HttpResponse::build(self.status_code()).json(error_response)
     }
 }
 
@@ -59,13 +57,23 @@ struct ErrorResponse {
     message: String,
 }
 
+#[derive(Serialize)]
+struct AccountResponse {
+    account: Address,
+    balance: U256,
+}
+
 #[get("/binance/token/{id}")]
 async fn owner_of(id: web::Path<i64>) -> Result<Json<Value>, CustomResponseErrors> {
     let t = web3::transports::Http::new("https://bsc-dataseed.binance.org/");
 
     let transport = match t {
         Ok(transport) => transport,
-        Err(_error) => return Err(ConnectionProblems(String::from("Connection problems to the blockchain")))
+        Err(_error) => {
+            return Err(ConnectionProblems(String::from(
+                "Connection problems to the blockchain",
+            )))
+        }
     };
 
     let web3 = web3::Web3::new(transport);
@@ -93,7 +101,7 @@ async fn owner_of(id: web::Path<i64>) -> Result<Json<Value>, CustomResponseError
 
     let owner = match o {
         Ok(address) => address,
-        Err(e) => return Err(InvalidToken(e))
+        Err(e) => return Err(InvalidToken(e)),
     };
 
     let token_uri: String = token_contract
@@ -109,6 +117,45 @@ async fn owner_of(id: web::Path<i64>) -> Result<Json<Value>, CustomResponseError
     })))
 }
 
+#[get("/api/local")]
+async fn local_accounts() -> Result<Json<Vec<AccountResponse>>, CustomResponseErrors> {
+    let t = web3::transports::Http::new("http://127.0.0.1:7545");
+    let transport = match t {
+        Ok(transport) => transport,
+        Err(_error) => {
+            return Err(ConnectionProblems(String::from(
+                "Connection problems to the blockchain",
+            )))
+        }
+    };
+
+    let web3 = web3::Web3::new(transport);
+
+    println!("Calling accounts.");
+    let accs = web3.eth().accounts().await;
+    let accounts = match accs {
+        Ok(accounts) => accounts,
+        Err(_error) => {
+            return Err(ConnectionProblems(String::from(
+                "Connection problems to the blockchain",
+            )))
+        }
+    };
+    // println!("Accounts: {:?}", accounts);
+    // accounts.push("00a329c0648769a73afac7f9381e08fb43dbea72".parse().unwrap());
+
+    let mut vec: Vec<AccountResponse> = Vec::new();
+    for account in accounts {
+        let balance = web3.eth().balance(account, None).await.unwrap();
+        vec.push(AccountResponse {
+            account: account,
+            balance: balance,
+        });
+    }
+
+    Ok(web::Json(vec))
+}
+
 #[get("/")]
 async fn index() -> Json<Value> {
     web::Json(json!({
@@ -118,11 +165,13 @@ async fn index() -> Json<Value> {
 
 #[actix_web::main] // or #[tokio::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new()
-        .service(owner_of)
-        .service(index)
-    )
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(owner_of)
+            .service(index)
+            .service(local_accounts)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
