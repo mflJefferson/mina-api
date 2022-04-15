9f5742bfdb0e9b9518b82e066ae5474a38c5480a
jeff@macbook <mfljefferson@gmail.com>
2022-04-14 23:51:57 -0300

    feat list address owners with pagination

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,9 @@ use std::fmt::{Display, Formatter};
 use std::str::FromStr;
 use actix_web::{App, HttpResponse, HttpServer, web, get, error, ResponseError};
 use actix_web::http::StatusCode;
-use actix_web::web::Json;
+use actix_web::web::{Json};
 use serde_json::{json, Value};
-use serde::{Serialize};
+use serde::{Serialize, Deserialize};
 use web3::contract::{Contract, Options};
 use web3::types::{Address, U256};
 use crate::CustomResponseErrors::{ConnectionProblems, InvalidToken};
@@ -59,6 +59,33 @@ struct ErrorResponse {
     message: String,
 }
 
+#[derive(Serialize)]
+struct Token {
+    token_id: U256,
+    owner_address: Address,
+}
+
+#[derive(Serialize)]
+struct TokensResponse {
+    tokens: Vec<Token>,
+    total_supply: U256
+}
+
+#[derive(Deserialize)]
+struct Pagination {
+    page: u32,
+    limit: u32
+}
+
+impl Default for Pagination {
+    fn default() -> Self {
+        Pagination {
+            page: 1,
+            limit: 10
+        }
+    }
+}
+
 #[get("/binance/token/{id}")]
 async fn owner_of(id: web::Path<i64>) -> Result<Json<Value>, CustomResponseErrors> {
     let t = web3::transports::Http::new("https://bsc-dataseed.binance.org/");
@@ -109,6 +136,58 @@ async fn owner_of(id: web::Path<i64>) -> Result<Json<Value>, CustomResponseError
     })))
 }
 
+#[get("/binance/tokens")]
+async fn tokens(pagination: web::Query<Pagination>) -> Result<Json<TokensResponse>, CustomResponseErrors> {
+    let t = web3::transports::Http::new("https://bsc-dataseed.binance.org/");
+
+    let first_result = (pagination.page - 1) * pagination.limit + 1;
+    let last_result = first_result + pagination.limit;
+    let transport = match t {
+        Ok(transport) => transport,
+        Err(_error) => return Err(ConnectionProblems(String::from("Connection problems to the blockchain")))
+    };
+
+    let web3 = web3::Web3::new(transport);
+
+    web3.eth();
+    let address = Address::from_str("0x1ec94be5c72cf0e0524d6ecb6e7bd0ba1700bf70").unwrap();
+    let token_contract =
+        Contract::from_json(web3.eth(), address, include_bytes!("contract-abi.json")).unwrap();
+
+    let total_supply: U256 = token_contract
+        .query("totalSupply", (), None, Options::default(), None)
+        .await
+        .unwrap();
+
+    let mut tokens: Vec<Token> = Vec::new();
+
+    for n in first_result..last_result {
+        let token_id: U256 = U256::from(n);
+        let o: Result<Address, web3::contract::Error> = token_contract
+            .query("ownerOf", token_id, None, Options::default(), None)
+            .await;
+
+        let owner = match o {
+            Ok(address) => address,
+            Err(e) => return Err(InvalidToken(e))
+        };
+
+        let token = Token {
+            token_id,
+            owner_address: owner
+        };
+
+        tokens.push(token);
+    }
+
+    let tokens_response = TokensResponse {
+        tokens,
+        total_supply
+    };
+
+    Ok(web::Json(tokens_response))
+}
+
 #[get("/")]
 async fn index() -> Json<Value> {
     web::Json(json!({
@@ -121,6 +200,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new()
         .service(owner_of)
         .service(index)
+        .service(tokens)
     )
         .bind(("127.0.0.1", 8080))?
         .run()
